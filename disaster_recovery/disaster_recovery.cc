97fcf74b758d63571931cb6c8870c5835892cc4c
Hongyi Zhang <hongyiz@google.com>
2019-01-21 17:28:05 -0800

    clean up test code before beta
    clean up test code. ServerConfigurableFlagsReset() should always be
    called.
    
    Test: m -j & manually on device
    Change-Id: Ib93b4f49ae034cdb5634fd36c85096ed30b84caa

--- disaster_recovery/disaster_recovery.cc
+++ disaster_recovery/disaster_recovery.cc
@@ -26,18 +26,7 @@ int main(int argc, char**) {
     return 1;
   }
 
-  // Reading flags is only for E2E testing in droidfood. Flags_health_check
-  // should always be enabled in release.
-  // Code for reading the flag will be removed before release.
-  // tracking bug: b/119627143
-  std::string flag_value = server_configurable_flags::GetServerConfigurableFlag(
-      "global_settings", "native_flags_health_check_enabled", "1");
-  if (flag_value == "1") {
-    LOG(INFO) << "Starting server configurable flags health check.";
-    server_configurable_flags::ServerConfigurableFlagsReset();
-  } else {
-    LOG(INFO) << "Server configurable flags health check is disabled";
-  }
+  server_configurable_flags::ServerConfigurableFlagsReset();
 
   return 0;
 }
